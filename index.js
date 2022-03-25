432a6250d7e5ff161f1420018b1a468bf1318e53
Sanderson Srisouraj <ssanderson295@gmail.com>
2022-03-25 13:35:09 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(haveFun = "roller-skate") {
+    return `This Saturday, I want to ${haveFun}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective (symbol="*") {
+    return function (adj = "special") {
+        return `You are ${symbol}${adj}${symbol}!`
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
